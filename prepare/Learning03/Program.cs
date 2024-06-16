066837967efaad78408a05792fd7ce8fdbd8e87e
ominosity <ominosity@msn.com>
2024-06-16 16:30:50 -0700

    Created Learning03 to work with fractions.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue().ToString());
+
+        fraction = new Fraction(6);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue().ToString());
+
+        fraction = new Fraction(6, 7);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue().ToString());  
+
+        fraction.SetTop(1);
+        fraction.SetBottom(1);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue().ToString());  
+
+        fraction.SetTop(5);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue().ToString());  
+
+        fraction.SetTop(3);
+        fraction.SetBottom(4);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue().ToString());  
+
+        fraction.SetTop(1);
+        fraction.SetBottom(3);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue().ToString());  
     }
 }
