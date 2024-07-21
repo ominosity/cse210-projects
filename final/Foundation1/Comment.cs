c28d5dd5a4c83ee27df7e08198a20d673b236fbe
ominosity <ominosity@msn.com>
2024-07-20 21:00:08 -0700

    Added comments to the classes

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -1,14 +1,26 @@
+/// <summary>
+/// Represents a comment on a video, with the commenter's name
+/// </summary>
 public class Comment 
 {
     private string _commenter;
     private string _commentText;
 
+    /// <summary>
+    /// Create a comment
+    /// </summary>
+    /// <param name="commenter">The person making the comment</param>
+    /// <param name="commentText">The text of the comment</param>
     public Comment(string commenter, string commentText)
     {
         _commenter = commenter;
         _commentText = commentText;
     }
 
+    /// <summary>
+    /// Get the details of the comment (commenter and text) with a tabbed line beneath it
+    /// </summary>
+    /// <returns>A string with the commenter, comment text, and a tabbed-in line beneath to separate it from other comments</returns>
     public string GetCommentDetails() 
     {
         string details = $"{_commenter}: {_commentText}\n";
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,6 +1,7 @@
-using System.Runtime.Versioning;
 using System.Text;
-
+/// <summary>
+/// Represents a YouTube video + list of comments
+/// </summary>
 public class Video 
 {
     private string _title;
@@ -8,6 +9,12 @@ public class Video
     private int _length;
     private List<Comment> _comments;
 
+    /// <summary>
+    /// Create a new Video
+    /// </summary>
+    /// <param name="title">The title of the video</param>
+    /// <param name="author">The author of the video</param>
+    /// <param name="length">The length of the video, in seconds</param>
     public Video(string title, string author, int length)
     {
         _title = title;
@@ -17,22 +24,38 @@ public class Video
         _comments = new List<Comment>();
     }
 
+    /// <summary>
+    /// Get the count of comments on this video
+    /// </summary>
+    /// <returns>An integer representing the number of comments on this video</returns>
     public int GetNumberOfComments()
     {
         return _comments.Count;
     }
 
+    /// <summary>
+    /// Add a comment to this video
+    /// </summary>
+    /// <param name="comment">A Comment object representing a comment on this video</param>
     public void AddComment(Comment comment)
     {
         _comments.Add(comment);
     }
 
+    /// <summary>
+    /// Get the single-line description of this video
+    /// </summary>
+    /// <returns>A string with the title, author, length in seconds, and count of comments</returns>
     public string GetDescription() 
     {
         string description = $"{_title} by {_author}, {_length} seconds, {GetNumberOfComments()} comments.";
         return description;
     }
 
+    /// <summary>
+    /// Get the list of comments as a string with newlines
+    /// </summary>
+    /// <returns>A string with c</returns>
     public string GetCommentList()
     {
         StringBuilder sb = new StringBuilder();
